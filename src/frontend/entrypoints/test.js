2bf09f034a545996f372ddeec3ecd8c9fb96f562
phillip <phillipc@ziprecruiter.com>
2020-11-29 23:41:57 -0800

    Fix entrypoint code
    Should've been fixed way earlier in a commit, whooooops

--- src/frontend/entrypoints/test.js
+++ src/frontend/entrypoints/test.js
@@ -1,5 +1,7 @@
+import React from 'react';
+
 import ReactDOM from 'react-dom';
 
 import Test from '../assets/Test';
 
-ReactDOM.render(document.querySelector('#test'), Test);
+ReactDOM.render(<Test />, document.getElementById('test'));
