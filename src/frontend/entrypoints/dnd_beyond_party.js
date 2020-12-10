9a25c57dab6ec7ce87d85ec67b1cd072b2d8bcab
phillip <phillipc@ziprecruiter.com>
2020-12-09 21:56:36 -0800

    Add web-vitals

--- src/frontend/entrypoints/dnd_beyond_party.js
+++ src/frontend/entrypoints/dnd_beyond_party.js
@@ -1,7 +1,14 @@
 import React from 'react';
-
 import ReactDOM from 'react-dom';
+import { getCLS, getFID, getLCP } from 'web-vitals';
 
 import App from '@assets/components/App';
+
+// eslint-disable-next-line no-console
+const consoleLog = console.log;
+
+getCLS(consoleLog);
+getFID(consoleLog);
+getLCP(consoleLog);
 
 ReactDOM.render(<App />, document.getElementById('test'));
