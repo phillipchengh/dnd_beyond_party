e298a6cea834a6076ccfdf03ada73456a1015f1e
phillip <phillipc@ziprecruiter.com>
2020-12-10 23:49:27 -0800

    Integrate localStorage with party state

--- src/frontend/assets/party/ducks.js
+++ src/frontend/assets/party/ducks.js
@@ -1,9 +1,30 @@
+// increment this when the state structure changes to force update stored values
+const schemaVersion = 1;
+const LOCAL_STORAGE_KEY = 'party';
+
+function getInitialState() {
+  try {
+    const localStorageState = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    // if our versions match, use the cached data
+    if (parseInt(localStorageState?.meta?.schemaVersion, 10) === schemaVersion) {
+      return localStorageState;
+    }
+  } catch (e) {
+    // guess this could happen if parse fails?
+  }
+  // empty case, start anew
+  return {
+    campaigns: {},
+    meta: {
+      schemaVersion,
+    },
+  };
+}
+
+export const initialState = getInitialState();
+
 export const ActionTypes = {
   IMPORT_CHARACTER: 'import_character',
-};
-
-export const initialState = {
-  campaigns: {},
 };
 
 export const actions = {
@@ -34,4 +55,14 @@ export function reducer(state = initialState, action) {
   }
 }
 
-export default reducer;
+// this will save state to localStorage, so if the page is revisited, initialState can get it
+const withLocalStorage = (wrappedReducer) => (
+  (state, action) => {
+    const nextState = wrappedReducer(state, action);
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(nextState));
+    return nextState;
+  }
+);
+
+// default export will use localStorage
+export default withLocalStorage(reducer);
