337087998429966781649c9e8d813b545932ea43
phillip <phillipc@ziprecruiter.com>
2020-12-10 23:24:34 -0800

    Add babel optional chaining, which required babel eslint parser and upgrade to eslint 7

--- babel.config.js
+++ babel.config.js
@@ -24,6 +24,7 @@ module.exports = function preset(api) {
     ],
     plugins: [
       'react-hot-loader/babel',
+      '@babel/plugin-proposal-optional-chaining',
     ],
   };
 };
--- src/frontend/.eslintrc.js
+++ src/frontend/.eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   env: {
     browser: true,
   },
+  parser: '@babel/eslint-parser',
   rules: {
     'react/jsx-filename-extension': [1, {
       extensions: ['.js', '.jsx'],
