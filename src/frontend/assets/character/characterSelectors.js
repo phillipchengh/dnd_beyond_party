2d9b0d2fd24e91a5a1372f53309d46cf6a1f937d
phillip <phillipc@ziprecruiter.com>
2020-12-12 22:27:24 -0800

    Add namespace import for character selectors
    Same named functions in party selectors causes issues

---
+++ src/frontend/assets/character/characterSelectors.js
@@ -0,0 +1,8 @@
+// Namespace import for files that have same function names that clash
+// https://medium.com/unsplash/named-namespace-imports-7345212bbffb
+
+import * as characterSelectors from './selectors';
+
+export { characterSelectors };
+
+export default { characterSelectors };
