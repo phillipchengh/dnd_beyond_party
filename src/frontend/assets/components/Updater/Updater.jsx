90d9bbe1263b59e9f0d388e787df966cb46b26f2
phillip <phillipc@ziprecruiter.com>
2020-12-21 21:29:53 -0800

    Manage errors in Updaters as well

--- src/frontend/assets/components/Updater/Updater.jsx
+++ src/frontend/assets/components/Updater/Updater.jsx
@@ -1,7 +1,14 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, {
+  useCallback,
+  useContext,
+  useEffect,
+  useState,
+} from 'react';
 
 import PartyContext from '@assets/party/Context';
+import { actions } from '@assets/party/ducks';
 import { updateCurrentCampaign } from '@assets/party/sideEffects';
+import { getError } from '@assets/party/selectors';
 
 // units in seconds
 const TEN_SECONDS = 10;
@@ -9,21 +16,37 @@ const ONE_MINUTE = 60;
 
 export function Updater() {
   const context = useContext(PartyContext);
+  const { dispatch, state } = context;
   const [secondsElapsed, setSecondsElapsed] = useState(0);
   const [autoUpdate, setAutoUpdate] = useState(null);
+  const [requestError, setRequestError] = useState(null);
 
-  const handleManualUpdate = async () => {
-    updateCurrentCampaign(context);
-    setSecondsElapsed(0);
+  const handleUpdate = useCallback(() => {
+    try {
+      updateCurrentCampaign(context);
+      setSecondsElapsed(0);
+    } catch (e) {
+      setRequestError(e.message);
+    }
+  }, [context]);
+
+  const clearErrors = () => {
+    dispatch(actions.clearError());
+    setRequestError(null);
   };
 
   const handleAutoUpdate = (intervalSeconds) => () => {
+    clearErrors();
     setAutoUpdate(intervalSeconds);
   };
 
+  const error = requestError || getError(state);
+  // don't continue auto update if there's an error
+  const runAutoUpdate = autoUpdate && !error;
+
   useEffect(() => {
     let intervalId = null;
-    if (autoUpdate) {
+    if (runAutoUpdate) {
       intervalId = setInterval(() => {
         // need to reference the previous seconds value due to stale state in useEffect
         setSecondsElapsed((prevSecondsElapsed) => (prevSecondsElapsed + 1));
@@ -35,24 +58,26 @@ export function Updater() {
         clearInterval(intervalId);
       }
     };
-  }, [autoUpdate]);
+  }, [runAutoUpdate]);
 
   useEffect(() => {
-    if (autoUpdate && secondsElapsed >= autoUpdate) {
-      updateCurrentCampaign(context);
-      setSecondsElapsed(0);
+    if (runAutoUpdate && secondsElapsed >= autoUpdate) {
+      handleUpdate();
     }
-  }, [autoUpdate, secondsElapsed, context]);
+  }, [autoUpdate, handleUpdate, runAutoUpdate, secondsElapsed]);
 
   return (
     <>
       <p>Current Campaign Updaters</p>
-      <button onClick={handleManualUpdate} type="button">Update Manually</button>
+      <button onClick={handleUpdate} type="button">Update Manually</button>
       <button onClick={handleAutoUpdate(TEN_SECONDS)} type="button">Auto-Update Every 10 Seconds</button>
       <button onClick={handleAutoUpdate(ONE_MINUTE)} type="button">Auto-Update Every Minute</button>
       <button onClick={handleAutoUpdate(null)} type="button">Turn off Auto-Update</button>
       {autoUpdate && <p>{`Auto updating current campaign in ${autoUpdate - secondsElapsed} seconds.`}</p>}
       <p>{`Waited ${secondsElapsed} seconds.`}</p>
+      {error && (
+        <p>{error}</p>
+      )}
     </>
   );
 }
