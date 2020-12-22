0c24df8e296fb447183145eb2a12aefc1ba26900
phillip <phillipc@ziprecruiter.com>
2020-12-22 14:58:14 -0800

    throw DnDBeyondError if service returns an error response
    Different logic is run if we run into this error

--- src/frontend/assets/api.js
+++ src/frontend/assets/api.js
@@ -1,10 +1,12 @@
-export async function getCharacter(id) {
-  // const init = {
+import DndBeyondError from './DndBeyondError';
 
-  // };
+export async function getCharacter(id) {
   const response = await fetch(`/character?id=${id}`);
-  // if (!response.ok || !/application\/json/.test(response.headers.get('Content-Type'))) {
-  if (!response.ok) {
+  if (!response.ok || !/application\/json/.test(response.headers.get('Content-Type'))) {
+    if (response.headers.get('Content-Type').includes('application/json')) {
+      const errorJson = await response.json();
+      throw new DndBeyondError('Retrieved an error with D&D Beyond.', errorJson);
+    }
     throw new Error('Failed getting character!');
   }
   const json = await response.json();
