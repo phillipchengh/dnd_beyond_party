22eee728a61f61e19dc21b7bb641c604a0c48c9f
phillip <phillipc@ziprecruiter.com>
2020-12-29 15:36:30 -0800

    Add ability to debug calcs and selectors from the browser console

--- src/frontend/assets/components/DnDBeyondPartyApp.jsx
+++ src/frontend/assets/components/DnDBeyondPartyApp.jsx
@@ -2,11 +2,19 @@ import React, { useReducer } from 'react';
 
 import partyReducer, { initialState } from '@assets/party/ducks';
 import PartyContext from '@assets/party/Context';
+import * as calcs from '@assets/character/calcs';
+import * as selectors from '@assets/party/selectors';
 
 import Main from './Main';
 
 export function DndBeyondPartyApp() {
   const [state, dispatch] = useReducer(partyReducer, initialState);
+
+  // for debugging calcs and selectors from the console
+  // TODO either remove this when done or read an environment value
+  window.state = state;
+  window.calcs = calcs;
+  window.selectors = selectors;
 
   return (
     <PartyContext.Provider
