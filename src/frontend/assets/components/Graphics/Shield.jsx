7c4854a6301217d879ab2c9f6743b1853a49cde3
phillip <phillipc@ziprecruiter.com>
2021-01-31 23:47:18 -0800

    Add component svg graphics

---
+++ src/frontend/assets/components/Graphics/Shield.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function Shield() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth={2}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="shield_svg"
+    >
+      <path d="M12 22s8.9-3.918 8.9-9.918v-7c-3.531.017-6.644-1.6-8.9-3.082-4.823 3.42-7.546 3.237-9.104 3.082v7C2.896 18.082 12 22 12 22z" />
+    </svg>
+  );
+}
+
+export default Shield;
