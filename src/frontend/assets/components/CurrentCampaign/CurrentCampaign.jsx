c45aa56f8f38ddf6243a41bbb0bd6a58ceee7c35
phillip <phillipc@ziprecruiter.com>
2021-02-22 20:36:46 -0800

    Update and style current campaign header

--- src/frontend/assets/components/CurrentCampaign/CurrentCampaign.jsx
+++ src/frontend/assets/components/CurrentCampaign/CurrentCampaign.jsx
@@ -1,10 +1,13 @@
 import React, { useContext, useState } from 'react';
 
+import formatDistanceToNow from 'date-fns/formatDistanceToNow';
+
 import PartyContext from '@assets/party/Context';
 
 import { actions } from '@assets/party/ducks';
 
 import {
+  getCurrentCampaignLastUpdate,
   getCurrentCampaignName,
   getCurrentCampaignLink,
   getSortedCurrentCampaignCharacters,
@@ -15,8 +18,10 @@ import {
   getId,
 } from '@assets/character/calcs';
 
-import Updater from '@assets/components/Updater/Updater';
 import Character from './Character';
+
+import ExternalLink from '../Graphics/ExternalLink';
+import Trash from '../Graphics/Trash';
 
 import './CurrentCampaign.less';
 
@@ -24,9 +29,11 @@ export function CurrentCampaign() {
   const { dispatch, state } = useContext(PartyContext);
 
   const showCurrentCampaign = hasCurrentCampaign(state);
+  let currentCampaignLastUpdate;
   let currentCampaignName;
   let currentCampaignCharacters;
   if (showCurrentCampaign) {
+    currentCampaignLastUpdate = getCurrentCampaignLastUpdate(state);
     currentCampaignName = getCurrentCampaignName(state);
     currentCampaignCharacters = getSortedCurrentCampaignCharacters(state);
   }
@@ -49,10 +56,19 @@ export function CurrentCampaign() {
       )}
       {showCurrentCampaign && (
         <>
-          <Updater />
-          <h2>{`Current Campaign: ${currentCampaignName}`}</h2>
-          <button onClick={handleDelete} type="button">Delete</button>
-          <a href={getCurrentCampaignLink(state)}>DnD Beyond Link</a>
+          <div className="title_container">
+            <a className="dndbeyond_link" href={getCurrentCampaignLink(state)}>
+              <h2 className="title">{currentCampaignName}</h2>
+              <ExternalLink />
+            </a>
+            <button className="delete_button" onClick={handleDelete} type="button">
+              <span className="delete_button_text">Delete</span>
+              <Trash />
+            </button>
+          </div>
+          <p className="last_update_text">
+            {`last updated ${formatDistanceToNow(currentCampaignLastUpdate)} ago`}
+          </p>
           <ol className="character_list">
             {currentCampaignCharacters.map(({ lastUpdate, data }) => (
               <li className="character_item" key={getId(data)}>
