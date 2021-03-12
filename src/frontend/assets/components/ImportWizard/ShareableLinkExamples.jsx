a88bbe1950f4d37b3e5ac105c7f0fcfd8c4e483f
phillip <phillipc@ziprecruiter.com>
2021-03-11 22:54:42 -0800

    Move shareable links to own component, to be put into a message

---
+++ src/frontend/assets/components/ImportWizard/ShareableLinkExamples.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import './ShareableLinkExamples.less';
+
+export function ShareableLinkExamples() {
+  return (
+    <div className="shareable_link_examples">
+      <p>Or a shareable link like one of these...</p>
+      <ul>
+        <li>
+          <a className="link" href="https://ddb.ac/characters/41160222/vj9Xaj">
+            https://ddb.ac/characters/41160222/vj9Xaj
+          </a>
+        </li>
+        <li>
+          <a className="link" href="https://www.dndbeyond.com/profile/Scrubbery/characters/41160222">
+            https://www.dndbeyond.com/profile/Scrubbery/characters/41160222
+          </a>
+        </li>
+        <li>
+          <a className="link" href="https://www.dndbeyond.com/characters/41160222">
+            https://www.dndbeyond.com/characters/41160222
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default ShareableLinkExamples;
