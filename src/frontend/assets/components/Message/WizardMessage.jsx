e9807ff4203f522384a46c1e1b1b8ae8b67f087f
phillip <phillipc@ziprecruiter.com>
2021-03-11 22:34:46 -0800

    Add scrollIntoView option for wizard message

--- src/frontend/assets/components/Message/WizardMessage.jsx
+++ src/frontend/assets/components/Message/WizardMessage.jsx
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 
 import Wand from '../Graphics/Wand';
 
 import './WizardMessage.less';
 
-export function WizardMessage({ className, children }) {
+export function WizardMessage({ className, children, scrollIntoView }) {
+  const container = useRef(null);
+
+  useEffect(() => {
+    if (scrollIntoView) {
+      container?.current.scrollIntoView({
+        behavior: 'smooth',
+        block: 'end',
+      });
+    }
+  }, [scrollIntoView]);
+
   return (
-    <div className={`wizard_message ${className}`}>
+    <div className={`wizard_message ${className}`} ref={container}>
       <div className="wand_wrapper">
         <Wand />
       </div>
@@ -24,10 +35,12 @@ WizardMessage.propTypes = {
     PropTypes.arrayOf(PropTypes.node),
     PropTypes.node,
   ]).isRequired,
+  scrollIntoView: PropTypes.bool,
 };
 
 WizardMessage.defaultProps = {
   className: '',
+  scrollIntoView: false,
 };
 
 export default WizardMessage;
