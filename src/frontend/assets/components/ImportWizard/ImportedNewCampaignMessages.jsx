6d5e16c0c8c35b570eb326412fa938a17b4e55b8
phillip <phillipc@ziprecruiter.com>
2021-03-11 22:56:15 -0800

    Add messages that streams each imported character from a campaign import

---
+++ src/frontend/assets/components/ImportWizard/ImportedNewCampaignMessages.jsx
@@ -0,0 +1,53 @@
+import React, { Fragment, useState } from 'react';
+import PropTypes from 'prop-types';
+
+import { getRaceClassDisplay, getId, getName } from '@assets/character/calcs';
+
+import Toggle from './Toggle';
+import WizardMessageDelay from '../Message/WizardMessageDelay';
+
+export function ImportedNewCampaignMessages({
+  campaignCharacters, onDone,
+}) {
+  const [
+    showCharacterMessageIndex,
+    setShowCharacterMessageIndex,
+  ] = useState(0);
+
+  const showNextCharacter = () => {
+    // we are done on the last character
+    if ((showCharacterMessageIndex + 1) >= campaignCharacters.length) {
+      onDone();
+    } else {
+      setShowCharacterMessageIndex((prevIndex) => (prevIndex + 1));
+    }
+  };
+
+  return (
+    <>
+      {campaignCharacters.map(({ data }, index) => {
+        const characterName = getName(data);
+        const characterRaceClass = getRaceClassDisplay(data);
+        // could be a newbie character, so no title
+        const characterTitle = characterRaceClass ? `the ${characterRaceClass} ` : '';
+
+        return (
+          <Fragment key={getId(data)}>
+            <Toggle show={index <= showCharacterMessageIndex}>
+              <WizardMessageDelay onDone={showNextCharacter}>
+                {`${characterName} ${characterTitle}has joined the fray!`}
+              </WizardMessageDelay>
+            </Toggle>
+          </Fragment>
+        );
+      })}
+    </>
+  );
+}
+
+ImportedNewCampaignMessages.propTypes = {
+  campaignCharacters: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
+  onDone: PropTypes.func.isRequired,
+};
+
+export default ImportedNewCampaignMessages;
