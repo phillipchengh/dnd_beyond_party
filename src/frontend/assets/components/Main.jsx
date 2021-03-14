361346e1f8db2964e923045cd69c94e754884806
phillip <phillipc@ziprecruiter.com>
2021-03-14 15:31:26 -0700

    Move main content into own component
    Welcome component will have a different, special view

--- src/frontend/assets/components/Main.jsx
+++ src/frontend/assets/components/Main.jsx
@@ -1,58 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-import Header from './Header/Header';
-import Nav from './Nav/Nav';
-import CurrentCampaign from './CurrentCampaign/CurrentCampaign';
-
-import './Main.less';
+import MainView from './MainView';
 
 export function Main() {
-  const [desktopNavOpen, setDesktopNavOpen] = useState(true);
-  const [mobileNavOpen, setMobileNavOpen] = useState(false);
-
-  const handleDesktopNavOpen = () => {
-    setDesktopNavOpen(true);
-  };
-
-  const handleDesktopNavClose = () => {
-    setDesktopNavOpen(false);
-  };
-
-  const handleMobileNavOpen = () => {
-    setMobileNavOpen(true);
-  };
-
-  const handleMobileNavClose = () => {
-    setMobileNavOpen(false);
-  };
-
-  const mainClasses = ['main'];
-  if (desktopNavOpen) {
-    mainClasses.push('desktop_nav_open');
-  } else {
-    mainClasses.push('desktop_nav_close');
-  }
-
   return (
     <>
-      <div className={mainClasses.join(' ')}>
-        <Header
-          desktopNavOpen={desktopNavOpen}
-          onDesktopNavOpen={handleDesktopNavOpen}
-          onMobileNavOpen={handleMobileNavOpen}
-        />
-        <main>
-          <CurrentCampaign />
-        </main>
-      </div>
-      <Nav
-        desktopOpen={desktopNavOpen}
-        mobileOpen={mobileNavOpen}
-        onDesktopClose={handleDesktopNavClose}
-        onDesktopOpen={handleDesktopNavOpen}
-        onMobileClose={handleMobileNavClose}
-        onMobileOpen={handleMobileNavOpen}
-      />
+      <MainView />
     </>
   );
 }
---
+++ src/frontend/assets/components/MainView.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from 'react';
+
+import Header from './Header/Header';
+import Nav from './Nav/Nav';
+import CurrentCampaign from './CurrentCampaign/CurrentCampaign';
+
+import './MainView.less';
+
+export function MainView() {
+  const [desktopNavOpen, setDesktopNavOpen] = useState(true);
+  const [mobileNavOpen, setMobileNavOpen] = useState(false);
+
+  const handleDesktopNavOpen = () => {
+    setDesktopNavOpen(true);
+  };
+
+  const handleDesktopNavClose = () => {
+    setDesktopNavOpen(false);
+  };
+
+  const handleMobileNavOpen = () => {
+    setMobileNavOpen(true);
+  };
+
+  const handleMobileNavClose = () => {
+    setMobileNavOpen(false);
+  };
+
+  const mainClasses = ['main_view'];
+  if (desktopNavOpen) {
+    mainClasses.push('desktop_nav_open');
+  } else {
+    mainClasses.push('desktop_nav_close');
+  }
+
+  return (
+    <>
+      <div className={mainClasses.join(' ')}>
+        <Header
+          desktopNavOpen={desktopNavOpen}
+          onDesktopNavOpen={handleDesktopNavOpen}
+          onMobileNavOpen={handleMobileNavOpen}
+        />
+        <main>
+          <CurrentCampaign />
+        </main>
+      </div>
+      <Nav
+        desktopOpen={desktopNavOpen}
+        mobileOpen={mobileNavOpen}
+        onDesktopClose={handleDesktopNavClose}
+        onDesktopOpen={handleDesktopNavOpen}
+        onMobileClose={handleMobileNavClose}
+        onMobileOpen={handleMobileNavOpen}
+      />
+    </>
+  );
+}
+
+export default MainView;
