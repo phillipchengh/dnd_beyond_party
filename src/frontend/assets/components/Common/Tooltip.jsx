ac940babde94baec9b80b19c02263c7f65cb28f4
phillip <phillipc@ziprecruiter.com>
2021-03-14 15:32:42 -0700

    Add popper class, to play well with facebook's z-index

--- src/frontend/assets/components/Common/Tooltip.jsx
+++ src/frontend/assets/components/Common/Tooltip.jsx
@@ -5,12 +5,13 @@ import { Tooltip as MaterialTooltip } from '@material-ui/core';
 
 import './Tooltip.less';
 
-export function Tooltip({ children, title }) {
+export function Tooltip({ children, popperClassName, title }) {
   return (
     <MaterialTooltip
       arrow
       classes={{
         arrow: 'arrow',
+        popper: `popper ${popperClassName}`,
         tooltip: 'tooltip',
       }}
       title={title}
@@ -21,11 +22,16 @@ export function Tooltip({ children, title }) {
 }
 
 Tooltip.propTypes = {
+  popperClassName: PropTypes.string,
   children: PropTypes.oneOfType([
     PropTypes.arrayOf(PropTypes.node),
     PropTypes.node,
   ]).isRequired,
   title: PropTypes.string.isRequired,
+};
+
+Tooltip.defaultProps = {
+  popperClassName: '',
 };
 
 export default Tooltip;
