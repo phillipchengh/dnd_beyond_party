7b58de29347328e9ad81a3d9cca652a4f6332f68
phillip <phillipc@ziprecruiter.com>
2021-03-13 18:06:39 -0800

    Add more descriptive error messages inside wizard

--- src/frontend/assets/DndBeyondError.js
+++ src/frontend/assets/DndBeyondError.js
@@ -1,6 +1,8 @@
 // DnDBeyondError signifies an error that DnD Beyond's service returns
+export const DEFAULT_MESSAGE = 'Apologies, I retrieved an error with D&D Beyond. Please check if your character is active and public in a running campaign.';
+
 export class DndBeyondError extends Error {
-  constructor(message = 'Retrieved an error with D&D Beyond.', response = {}) {
+  constructor(message = DEFAULT_MESSAGE, response = {}) {
     super(message);
     this.response = response;
   }
--- src/frontend/assets/api.js
+++ src/frontend/assets/api.js
@@ -1,13 +1,15 @@
-import DndBeyondError from './DndBeyondError';
+import DndBeyondError, { DEFAULT_MESSAGE } from './DndBeyondError';
+
+const DEFAULT_ERROR_MESSAGE = 'Apologies, something went wrong. Please try again.';
 
 export async function getCharacter(id) {
   const response = await fetch(`/character?id=${id}`);
   if (!response.ok || !/application\/json/.test(response.headers.get('Content-Type'))) {
     if (response.headers.get('Content-Type').includes('application/json')) {
       const errorJson = await response.json();
-      throw new DndBeyondError('Retrieved an error with D&D Beyond.', errorJson);
+      throw new DndBeyondError(DEFAULT_MESSAGE, errorJson);
     }
-    throw new Error('Failed getting character!');
+    throw new Error(DEFAULT_ERROR_MESSAGE);
   }
   const json = await response.json();
   return json.data;
