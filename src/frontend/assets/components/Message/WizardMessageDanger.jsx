3fe64d2262a489edc0059bd38b0b69015af616e8
phillip <phillipc@ziprecruiter.com>
2021-03-14 16:21:18 -0700

    Allow adding class names in danger message

--- src/frontend/assets/components/Message/WizardMessageDanger.jsx
+++ src/frontend/assets/components/Message/WizardMessageDanger.jsx
@@ -5,15 +5,26 @@ import WizardMessage from './WizardMessage';
 
 import './WizardMessageDanger.less';
 
-export function WizardMessageDanger({ children }) {
-  return <WizardMessage className="wizard_message_danger">{children}</WizardMessage>;
+export function WizardMessageDanger({ className, children }) {
+  return (
+    <WizardMessage
+      className={`wizard_message_danger ${className}`}
+    >
+      {children}
+    </WizardMessage>
+  );
 }
 
 WizardMessageDanger.propTypes = {
+  className: PropTypes.string,
   children: PropTypes.oneOfType([
     PropTypes.arrayOf(PropTypes.node),
     PropTypes.node,
   ]).isRequired,
+};
+
+WizardMessageDanger.defaultProps = {
+  className: '',
 };
 
 export default WizardMessageDanger;
