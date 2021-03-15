9dfd4aeec986ecae4d573b7acb538f0695fbe179
phillip <phillipc@ziprecruiter.com>
2021-03-14 22:40:02 -0700

    Add no internet error message

--- src/frontend/assets/api.js
+++ src/frontend/assets/api.js
@@ -3,7 +3,12 @@ import DndBeyondError, { DEFAULT_MESSAGE } from './DndBeyondError';
 const DEFAULT_ERROR_MESSAGE = 'Apologies, something went wrong. Please try again.';
 
 export async function getCharacter(id) {
-  const response = await fetch(`/character?id=${id}`);
+  let response;
+  try {
+    response = await fetch(`/character?id=${id}`);
+  } catch (error) {
+    throw new Error('I had a problem connecting to the internet. Please check your connection and try again.');
+  }
   if (!response.ok || !/application\/json/.test(response.headers.get('Content-Type'))) {
     if (response.headers.get('Content-Type').includes('application/json')) {
       const errorJson = await response.json();
