116a5524c5f5477952ac8939a746a23658380b67
phillip <phillipc@ziprecruiter.com>
2021-03-21 13:08:10 -0700

    Untoggle web vitals
    Retoggle them for debugging

--- src/frontend/entrypoints/dnd_beyond_party.js
+++ src/frontend/entrypoints/dnd_beyond_party.js
@@ -4,15 +4,15 @@ import 'regenerator-runtime/runtime';
 
 import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
-import { getCLS, getFID, getLCP } from 'web-vitals';
+// import { getCLS, getFID, getLCP } from 'web-vitals';
 
 import App from '@assets/components/App';
 
 // eslint-disable-next-line no-console
-const consoleLog = console.log;
+// const consoleLog = console.log;
 
-getCLS(consoleLog);
-getFID(consoleLog);
-getLCP(consoleLog);
+// getCLS(consoleLog);
+// getFID(consoleLog);
+// getLCP(consoleLog);
 
 ReactDOM.render(<StrictMode><App /></StrictMode>, document.getElementById('dnd_beyond_party_app'));
